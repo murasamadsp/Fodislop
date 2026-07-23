04451d3b2124824f36cee1cf548f546f80ae29bd
Mykhailo Ostrovskyi <murasama.off@gmail.com>
2026-07-23 13:15:33 +0200

    fix: bind RobotHeadlight position and direction to active PlayerMovementController position

--- Assets/Scripts/Game/RobotHeadlight.cs
+++ Assets/Scripts/Game/RobotHeadlight.cs
@@ -32,6 +32,11 @@ namespace Fodinae.Scripts.Game
             Shader.SetGlobalFloat("_HeadlightIntensity", 0f);
         }
 
+        protected void OnDestroy()
+        {
+            Shader.SetGlobalFloat("_HeadlightIntensity", 0f);
+        }
+
         protected void LateUpdate()
         {
             if (!_isEnabled)
@@ -39,9 +44,11 @@ namespace Fodinae.Scripts.Game
                 return;
             }
 
-            Vector2 dir = Vector2.up;
+            Vector2 dir;
+            Vector2 pos;
+
             var player = PlayerMovementController.LocalPlayer;
-            if (player != null && (player.gameObject == gameObject || transform.IsChildOf(player.transform)))
+            if (player != null)
             {
                 dir = player.LastDirection switch
                 {
@@ -51,14 +58,14 @@ namespace Fodinae.Scripts.Game
                     Direction.Left => Vector2.left,
                     _ => Vector2.up
                 };
+                pos = (Vector2)player.transform.position + (dir * 0.5f);
             }
             else
             {
                 float angleRad = transform.eulerAngles.z * Mathf.Deg2Rad;
                 dir = new Vector2(-Mathf.Sin(angleRad), Mathf.Cos(angleRad));
+                pos = (Vector2)transform.position + (dir * 0.5f);
             }
-
-            Vector2 pos = (Vector2)transform.position + (dir * 0.5f);
 
             Shader.SetGlobalVector("_HeadlightPos", pos);
             Shader.SetGlobalVector("_HeadlightDir", dir);
